86b95300532c2160da4140bdf4a89ddec5e53a6a
Abijah Ford <ford.abijah@gmail.com>
2025-12-10 17:50:20 -0600

    updated packages

---
+++ src/api/anthropic.js
@@ -0,0 +1,33 @@
+import Anthropic from "@anthropic-ai/sdk";
+
+const SYSTEM_PROMPT = `
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page
+`;
+
+export default async function handler(req, res) {
+  try {
+    const { ingredients } = req.body;
+    const ingredientsString = ingredients.join(", ");
+
+    const anthropic = new Anthropic({
+      apiKey: process.env.CLAUDE_API_KEY,
+    });
+
+    const msg = await anthropic.messages.create({
+      model: "claude-3-haiku-20240307",
+      max_tokens: 1024,
+      system: SYSTEM_PROMPT,
+      messages: [
+        {
+          role: "user",
+          content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
+        },
+      ],
+    });
+
+    res.status(200).json({ recipe: msg.content[0].text });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Claude request failed" });
+  }
+}
