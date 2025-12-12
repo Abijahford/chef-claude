548d11fb1f08d2e7677d27db4f88e2f826f30e91
Abijah Ford <ford.abijah@gmail.com>
2025-12-12 11:50:25 -0600

    Added Instructions component, styling and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import React from "react";
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
+import Instructions from "./components/Instructions.jsx";
 export default function App() {
+  const [resume, setResume] = React.useState(false);
+  function getResume() {
+    setResume((prevResume) => !prevResume);
+  }
   return (
     <>
+      {!resume && <Instructions getResume={getResume} />}
       <Header />
       <Main />
     </>
---
+++ src/components/Instructions.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+export default function Instructions(props) {
+  // when continute button is clicked set state to true
+  return (
+    <div className="instructions">
+      <h1>Instructions</h1>
+      <p>
+        Enter <b>4 or more ingredients</b>.
+        <br />
+        Click the <span>Get Recipe</span> button to generate your recipe.
+      </p>
+      <button onClick={props.getResume}>Continue</button>
+    </div>
+  );
+}
